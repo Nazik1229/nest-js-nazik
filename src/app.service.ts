c9cf291146a7015b2ccf2031d39470f95c33c5af
Nazik1229 <torogulovanaz06@gmail.com>
2024-01-22 13:48:11 +0600

    add new point

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,14 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  sumOfEven() {
+    const arr = [1,2,3,4,5,6]
+    const sum = arr.reduce((accum, curEl) => {
+      if (curEl % 2 === 0) accum +=curEl;
+      return accum;
+    }, 0)
+
+    return sum
+  }
 }
